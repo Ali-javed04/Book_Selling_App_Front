24c9a3b8a9ebc62d8d851c181f1037ed785b1b25
mymacbook <stylemarostyle43@gmail.com>
2020-02-29 17:22:32 +0500

    ali

--- app/src/main/java/aust/fyp/learn/and/booksellingapp/StoreRoom/URLs.kt
+++ app/src/main/java/aust/fyp/learn/and/booksellingapp/StoreRoom/URLs.kt
@@ -3,12 +3,12 @@ package aust.fyp.learn.and.booksellingapp.StoreRoom
 object URLs {
 
     val IP_ADDRESS = "192.168.43.169"
-    val PORT = ":8080"
+    val PORT = "3012"
     val PROTOCOL = "http://"
 
     // users
     val CREATE_ACCOUNT = PROTOCOL + IP_ADDRESS + PORT + "/users/create"
-    val LOGIN = PROTOCOL + IP_ADDRESS + PORT + "/users/login"
+    val LOGIN = PROTOCOL + IP_ADDRESS + PORT + "/users/sign_in"
 
     fun getImageUrl(image_name: String?): String {
         return PROTOCOL + IP_ADDRESS + PORT + "/uploadedFiles/$image_name"
